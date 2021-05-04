1785a6108054bc5979d085f67a175abcf589f44f
antho1404 <anthony.estebe@gmail.com>
2021-05-04 17:58:35 +0700

    update vision block

--- components/vision/vision.tsx
+++ components/vision/vision.tsx
@@ -1,20 +1,20 @@
+import Container from "../container/container";
+
 export default function Vision() {
   return (
-    <div className="bg-white">
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <div className="text-center">
-          <h2 className="text-base font-semibold text-primary-600 tracking-wide uppercase">
-            Quiver Community Vision
-          </h2>
-          <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            An Innovative Way to Value Information
-          </p>
-          <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-            magnam voluptatum cupiditate veritatis in, accusamus quisquam.
-          </p>
-        </div>
+    <Container>
+      <div className="text-center p-10">
+        <h2 className="text-base font-semibold leading-6 text-primary tracking-wide uppercase">
+          Quiver Community Vision
+        </h2>
+        <p className="mt-3 text-4xl leading-10 font-extrabold text-purple-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+          An Innovative Way to Value Information
+        </p>
+        <p className="max-w-xl mt-6 leading-7 mx-auto text-xl text-purple-900">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          magnam voluptatum cupiditate veritatis in, accusamus quisquam.
+        </p>
       </div>
-    </div>
+    </Container>
   );
 }
