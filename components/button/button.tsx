125476c53c2d76eab616421eceeddbc3ab3af3ea
antho1404 <anthony.estebe@gmail.com>
2021-05-04 17:58:12 +0700

    add text button

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -7,18 +7,29 @@ type IProps = {
   shadow?: boolean;
   icon?: any;
   large?: boolean;
+  text?: boolean;
   className?: string;
 };
 
 export default function Button(props: PropsWithChildren<IProps>) {
   const structure =
     "whitespace-nowrap inline-flex items-center justify-center rounded-full text-base";
-  const size = props.large ? "px-6 py-3" : "px-4 py-2";
+  const px = props.text ? "" : props.large ? "px-6" : "px-4";
+  const py = props.large ? "py-3" : "py-2";
+  const size = `${px} ${py}`;
   const border = props.outlined ? "ring-2 ring-inset ring-primary" : null;
   const color = props.outlined
     ? "text-primary bg-white"
+    : props.text
+    ? "text-primary"
     : "text-white bg-gradient-to-r from-primary to-secondary";
-  const shadow = props.shadow ? "shadow-xl" : null;
+  const shadow = props.shadow && !props.text ? "shadow-xl" : null;
+  const icon = props.text
+    ? classNames(
+        "rounded-full text-white bg-gradient-to-r from-primary to-secondary h-8 w-8 mr-3 p-2",
+        props.shadow ? "shadow-xl" : null
+      )
+    : "mr-2";
   return (
     <a
       href={props.href}
@@ -32,7 +43,9 @@ export default function Button(props: PropsWithChildren<IProps>) {
       )}
     >
       {props.icon && (
-        <props.icon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+        <div className={classNames("flex justify-center align-middle", icon)}>
+          <props.icon className={classNames("h-4 w-4")} aria-hidden="true" />
+        </div>
       )}
       {props.children}
     </a>
