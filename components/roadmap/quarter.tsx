a465152d8b7a8cf8f565046a8f795d09ccb09b70
antho1404 <anthony.estebe@gmail.com>
2021-05-06 16:50:41 +0700

    improve roadmap

--- components/roadmap/quarter.tsx
+++ components/roadmap/quarter.tsx
@@ -1,24 +1,29 @@
+import { Fragment } from "react";
 import IconArrowLarge from "../icon/arrow-large";
 import { IQuarter } from "./types";
 
 export default function Quarter(quarter: IQuarter) {
   return (
     <div className="text-left" key={quarter.quarter}>
-      <span className="bg-purple-100 p-3 rounded-lg text-lg leading-6 font-semibold">
-        {quarter.quarter}
-      </span>
-      <IconArrowLarge />
+      <div className="flex">
+        <span className="mr-6 bg-purple-100 p-3 rounded-lg text-lg leading-6 font-semibold">
+          {quarter.quarter}
+        </span>
+        <IconArrowLarge className="inline mt-4" />
+      </div>
       <div className="pl-12 pt-6">
-        <ul className="ml-6">
-          {quarter.steps.map((x) => (
-            <li
-              key={x.title}
-              className="mb-8 text-purple-900 text-base leading-6 font-normal"
-            >
-              <span className="text-2xl mr-3">{x.icon}</span> {x.title}
-            </li>
+        <dl className="ml-6">
+          {quarter.steps.map((x, i) => (
+            <Fragment key={i}>
+              <dt className="float-left">
+                <span className="text-2xl mr-3">{x.icon}</span>
+              </dt>
+              <dd className="mb-8 text-purple-900 text-base leading-6 font-normal">
+                {x.title}
+              </dd>
+            </Fragment>
           ))}
-        </ul>
+        </dl>
       </div>
     </div>
   );
