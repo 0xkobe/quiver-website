602de9a9925ec065fa848a57f301b42b41d8728b
antho1404 <anthony.estebe@gmail.com>
2021-05-06 12:01:16 +0700

    update navigation

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -5,6 +5,7 @@ import { INavigationItem } from "./types";
 import Item from "./item";
 import Button from "../button/button";
 import Container from "../container/container";
+import Logo from "../logo/logo";
 
 const action: INavigationItem = {
   key: "whitepaper",
@@ -14,9 +15,9 @@ const action: INavigationItem = {
 
 const navigation: INavigationItem[] = [
   {
-    key: "vision",
-    name: "Vision",
-    href: "#vision",
+    key: "protocol",
+    name: "Protocol",
+    href: "#protocol",
   },
   {
     key: "ecosystem",
@@ -24,14 +25,24 @@ const navigation: INavigationItem[] = [
     href: "#ecosystem",
   },
   {
-    key: "nft",
-    name: "Emotional NFTs",
-    href: "#nft",
+    key: "invest",
+    name: "Invest",
+    href: "#invest",
   },
   {
     key: "roadmap",
     name: "Roadmap",
     href: "#roadmap",
+  },
+  {
+    key: "blog",
+    name: "Blog",
+    href: "#blog",
+  },
+  {
+    key: "careers",
+    name: "Careers",
+    href: "#careers",
   },
 ];
 
@@ -40,26 +51,25 @@ export default function Navigation() {
     <Popover>
       {({ open }) => (
         <>
-          <Container className="flex justify-between items-center md:justify-start md:space-x-10" padding={6}>
+          <Container
+            className="flex justify-between items-center lg:justify-start lg:space-x-10"
+            padding={6}
+          >
             <div className="flex justify-start lg:w-0 lg:flex-1">
               <a href="https://quiverprotocol.com">
                 <span className="sr-only">Quiver</span>
-                <img
-                  className="w-auto h-8 sm:h-10"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                  alt="Quiver"
-                />
+                <Logo className="w-auto h-8 sm:h-10" />
               </a>
             </div>
-            <nav className="hidden md:flex space-x-10">
+            <nav className="hidden lg:flex space-x-10">
               {navigation.map(Item)}
             </nav>
-            <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            <div className="hidden lg:flex items-center justify-end lg:flex-1 lg:w-0">
               <Button href={action.href} icon={DocumentTextIcon} outlined>
                 {action.name}
               </Button>
             </div>
-            <div className="-mr-2 -my-2 md:hidden">
+            <div className="-mr-2 -my-2 lg:hidden">
               <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500">
                 <span className="sr-only">Open menu</span>
                 <MenuIcon className="h-6 w-6" aria-hidden="true" />
@@ -80,18 +90,12 @@ export default function Navigation() {
             <Popover.Panel
               focus
               static
-              className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-10"
+              className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right lg:hidden z-10"
             >
               <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
                 <div className="pt-5 pb-6 px-5">
                   <div className="flex items-center justify-between">
-                    <div>
-                      <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt="Workflow"
-                      />
-                    </div>
+                    <Logo className="w-auto h-8" />
                     <div className="-mr-2">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500">
                         <span className="sr-only">Close menu</span>
