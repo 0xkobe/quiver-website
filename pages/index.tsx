6104bf5a422dd81d51bd8d9b76822bcf317275c8
antho1404 <anthony.estebe@gmail.com>
2021-05-06 18:55:48 +0700

    add metadata in head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Hero from "../components/hero/hero";
 import Information from "../components/information/information";
 import Features from "../components/features/features";
@@ -60,6 +61,16 @@ const members: IMember[] = [
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>
+          Quiver Protocol - 1st Community-driven Blockchain Investment Protocol
+        </title>
+        <meta
+          name="description"
+          content="Explore Investment opportunities market driven by the Quiver Community. Find the most profitable information and improve investment decisions, fast, easy and secure."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Hero members={members} />
       <Information />
       <Features />
