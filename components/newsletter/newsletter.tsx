8a10f4f3b80d319218ac2008506e9fd1ddd45c87
antho1404 <anthony.estebe@gmail.com>
2021-05-07 19:12:32 +0700

    add newsletter form

--- components/newsletter/newsletter.tsx
+++ components/newsletter/newsletter.tsx
@@ -7,15 +7,23 @@ type IProps = {
 
 export default function Newsletter(props: PropsWithChildren<IProps>) {
   const [email, setEmail] = useState("");
+
   const submit = (e) => {
-    console.log(e);
+    e.preventDefault();
+    document.querySelector("form").submit();
   };
 
   return (
-    <form className={props.className}>
+    <form
+      className={props.className}
+      name="contact"
+      method="POST"
+      data-netlify="true"
+    >
       <div className="lg:grid lg:grid-cols-4 lg:grid-flow-col-dense lg:gap-3">
         <input
           type="email"
+          name="email"
           className="col-span-3 w-full p-3 border border-purple-200 rounded-full text-purple-900 text-base leading-6 font-medium focus:outline-none"
           placeholder="Enter your email"
           value={email}
