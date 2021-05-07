4f8949a8d10170d6c5c1959e2e115356ad8c875b
antho1404 <anthony.estebe@gmail.com>
2021-05-07 18:57:09 +0700

    use bg-clip-text

--- components/text/headline.tsx
+++ components/text/headline.tsx
@@ -3,7 +3,8 @@ import GenerateTextComponent from "./_generator";
 const Headline = GenerateTextComponent({
   tag: "span",
   className: "text-base leading-6 font-semibold tracking-wide uppercase",
-  color: "text-gradient bg-gradient-to-r from-primary to-secondary",
+  color:
+    "bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary",
 });
 
 export default Headline;
