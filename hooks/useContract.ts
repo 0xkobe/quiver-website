9f7ec8e5bf37868e1b16e1d61111d8a14a71895d
antho1404 <anthony.estebe@gmail.com>
2021-06-11 16:28:47 +0700

    add usecontract hook

---
+++ hooks/useContract.ts
@@ -0,0 +1,23 @@
+import { Contract, ContractInterface } from '@ethersproject/contracts'
+import { Web3Provider } from '@ethersproject/providers'
+import { useWeb3React } from '@web3-react/core'
+import { useEffect, useState } from 'react'
+
+export default function useContract<T extends Contract>(
+  address: string,
+  abi: ContractInterface,
+): T {
+  const { library } = useWeb3React<Web3Provider>()
+  const [contract, setContract] = useState<T>()
+
+  useEffect(() => {
+    if (!library) return
+    const contract = new Contract(address, abi, library)
+    setContract(contract as T)
+    return () => {
+      setContract(undefined)
+    }
+  }, [library, address, abi])
+
+  return contract
+}
