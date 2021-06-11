c556d4b89f10677b06c9e2af958582e36dda428a
antho1404 <anthony.estebe@gmail.com>
2021-06-11 16:28:58 +0700

    ass useQSTKSale hook

---
+++ hooks/useQSTKSale.ts
@@ -0,0 +1,50 @@
+import { BigNumber } from '@ethersproject/bignumber'
+import { Signer } from 'ethers'
+import { useCallback, useEffect, useState } from 'react'
+import { abi, address } from '../contract'
+import useContract from './useContract'
+
+export default function useQSTKSale() {
+  const contract = useContract(address, abi)
+  const [price, setPrice] = useState<BigNumber>(BigNumber.from(0))
+  const [started, setStarted] = useState<boolean>(false)
+  const [error, setError] = useState<Error>()
+
+  useEffect(() => {
+    if (!contract) return
+    contract.qstkPrice().then(setPrice).catch(setError)
+  }, [contract, setPrice])
+
+  useEffect(() => {
+    if (!contract) return
+    contract.started().then(setStarted).catch(setError)
+  }, [contract, setStarted])
+
+  const getEthPrice = useCallback(
+    (qstkAmount: BigNumber) => {
+      if (!qstkAmount) return BigNumber.from(0)
+      if (!price) return BigNumber.from(0)
+      return qstkAmount.mul(price)
+    },
+    [price],
+  )
+
+  const purchase = useCallback(
+    (qstkAmount: BigNumber, signer: Signer) => {
+      return contract
+        .connect(signer)
+        .buyQStk(qstkAmount.mul(BigNumber.from(10).pow(18)), {
+          value: getEthPrice(qstkAmount),
+        })
+    },
+    [contract, getEthPrice],
+  )
+
+  return {
+    started,
+    error,
+    price,
+    getEthPrice,
+    purchase,
+  }
+}
